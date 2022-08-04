e89ceb32e26fe0a21a6cf69229a4449d54dde539
Eduard Caraenache <Eduard.CARAENACHE@frequentis.com>
2022-08-04 13:10:42 +0300

    Barosan

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import mypackage.Hi;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Hi.sayHi();
+        Hi.sayName();
+        Hi.sayBye();
+    }
+
+}
---
+++ src/mypackage/Hi.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class Hi {
+
+    public static void sayHi(){
+        System.out.println("Hi");
+    }
+    public static void sayName(){
+        System.out.println("I am Edu");
+    }
+    public static void sayBye(){
+        System.out.println("Ceao");
+    }
+
+}
